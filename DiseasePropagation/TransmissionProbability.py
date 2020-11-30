39edce79a4ff4cafc49c7a8aeb5f1d0d62bcdcd4
Huy <nqhuy213@gmail.com>
2020-12-01 09:48:23 +1000

    Update SIGMA

--- DiseasePropagation/TransmissionProbability.py
+++ DiseasePropagation/TransmissionProbability.py
@@ -3,5 +3,5 @@ import numpy as np
 from .Exposure import calculateExposure
 
 def calculateTransProbability(exposure):
-    SIGMA = 0.33  # This is for Influenza, needed to be change into Covid-19
+    SIGMA = np.random.normal(1, 0.1)  # This is for Influenza, needed to be change into Covid-19
     return 1 - math.exp(-SIGMA * exposure)
